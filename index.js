7619a6e6031b46990ea858788b1f64934f6156d1
Marc Neander <marc.neander@schibsted.se>
2018-10-22 22:10:22 +0200

    Simply babel-config even more

--- index.js
+++ index.js
@@ -5,37 +5,27 @@ const pluginProposalObjectRestSpread = require('@babel/plugin-proposal-object-re
 const pluginProposalThrowExpressions = require('@babel/plugin-proposal-throw-expressions');
 const pluginProposalClassProperties = require('@babel/plugin-proposal-class-properties');
 const pluginSyntaxDynamicImport = require('@babel/plugin-syntax-dynamic-import');
-const pluginTransformReactConstantElements = require('@babel/plugin-transform-react-constant-elements');
-const pluginTransformReactInlineElements = require('@babel/plugin-transform-react-inline-elements');
-const pluginTransformReactRemovePropTypes = require('babel-plugin-transform-react-remove-prop-types');
 
-module.exports = declare((api, opts) => {
+module.exports = declare(api => {
+    api.assertVersion(7);
     const isTest = api.env('test');
     const isProd = api.env('production');
-
-    const options = Object.assign(
-        {
-            react: true,
-            node: false
-        },
-        opts
-    );
 
     return {
         presets: [
             [
                 presetEnv,
                 {
-                    ...(options.node || isTest ? { targets: { node: 'current' } } : {})
+                    ...(isTest ? { targets: { node: 'current' } } : {})
                 }
             ],
-            options.react && [
+            [
                 presetReact,
                 {
                     development: !isProd
                 }
             ]
-        ].filter(Boolean),
+        ],
         plugins: [
             // Proposal
             pluginProposalObjectRestSpread,
@@ -43,12 +33,7 @@ module.exports = declare((api, opts) => {
             pluginProposalClassProperties,
 
             // Syntax
-            pluginSyntaxDynamicImport,
-
-            // Transforms
-            isProd && options.react && pluginTransformReactConstantElements,
-            isProd && options.react && pluginTransformReactInlineElements,
-            isProd && options.react && pluginTransformReactRemovePropTypes
+            pluginSyntaxDynamicImport
         ].filter(Boolean)
     };
 });
